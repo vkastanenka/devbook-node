05c24a3f8f008d2006f6d0c69867517f5325ec15
Victoria Kastanenka <vkastanenka@gmail.com>
2024-10-23 20:48:15 -0400

    updated schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,49 +1,49 @@
-import prisma from '../src/lib/db'
+// import prisma from '../src/lib/db'
 
-import { seedFn as postSeedFn, deleteFn as postDeleteFn } from './seeds/posts'
-import {
-  seedFn as sessionSeedFn,
-  deleteFn as sessionDeleteFn,
-} from './seeds/sessions'
-import { seedFn as userSeedFn, deleteFn as userDeleteFn } from './seeds/users'
+// import { seedFn as postSeedFn, deleteFn as postDeleteFn } from './seeds/posts'
+// import {
+//   seedFn as sessionSeedFn,
+//   deleteFn as sessionDeleteFn,
+// } from './seeds/sessions'
+// import { seedFn as userSeedFn, deleteFn as userDeleteFn } from './seeds/users'
 
-const deleteDatabase = async () => {
-  try {
-    // Posts
-    await Promise.all(postDeleteFn(prisma))
-    console.log('--- Deleted Posts ---')
+// const deleteDatabase = async () => {
+//   try {
+//     // Posts
+//     await Promise.all(postDeleteFn(prisma))
+//     console.log('--- Deleted Posts ---')
 
-    // Sessions
-    await Promise.all(sessionDeleteFn(prisma))
-    console.log('--- Deleted Sessions ---')
+//     // Sessions
+//     await Promise.all(sessionDeleteFn(prisma))
+//     console.log('--- Deleted Sessions ---')
 
-    // Users
-    await Promise.all(userDeleteFn(prisma))
-    console.log('--- Deleted Users ---')
-  } catch (error) {
-    console.warn('Please define your seed data.')
-    console.error(error)
-  }
-}
+//     // Users
+//     await Promise.all(userDeleteFn(prisma))
+//     console.log('--- Deleted Users ---')
+//   } catch (error) {
+//     console.warn('Please define your seed data.')
+//     console.error(error)
+//   }
+// }
 
-const seedDatabase = async () => {
-  try {
-    // Users
-    const users = await Promise.all(userSeedFn(prisma))
-    console.log('--- Seeded Users ---', users)
+// const seedDatabase = async () => {
+//   try {
+//     // Users
+//     const users = await Promise.all(userSeedFn(prisma))
+//     console.log('--- Seeded Users ---', users)
     
-    // Posts
-    const posts = await Promise.all(postSeedFn(prisma))
-    console.log('--- Seeded Posts ---', posts)
-  } catch (error) {
-    console.warn('Please define your seed data.')
-    console.error(error)
-  }
-}
+//     // Posts
+//     const posts = await Promise.all(postSeedFn(prisma))
+//     console.log('--- Seeded Posts ---', posts)
+//   } catch (error) {
+//     console.warn('Please define your seed data.')
+//     console.error(error)
+//   }
+// }
 
-const main = async () => {
-  await deleteDatabase()
-  // await seedDatabase()
-}
+// const main = async () => {
+//   await deleteDatabase()
+//   // await seedDatabase()
+// }
 
-main()
+// main()
--- prisma/seeds/posts.ts
+++ prisma/seeds/posts.ts
@@ -1,28 +1,28 @@
-import type { Prisma } from '@prisma/client'
-import { ExtendedPrismaClient } from '../../src/lib/db'
+// import type { Prisma } from '@prisma/client'
+// import { ExtendedPrismaClient } from '../../src/lib/db'
 
-const posts: Prisma.PostCreateArgs['data'][] = [
-  {
-    id: '8e3399e6-1d94-11ec-9621-0242ac130002',
-    body: '🌐 Just launched my latest full stack web app, and I couldn’t be more thrilled! 🎉 It’s a task management tool designed to enhance team collaboration and boost productivity.',
-    userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
-  },
-]
+// const posts: Prisma.PostCreateArgs['data'][] = [
+//   {
+//     id: '8e3399e6-1d94-11ec-9621-0242ac130002',
+//     body: '🌐 Just launched my latest full stack web app, and I couldn’t be more thrilled! 🎉 It’s a task management tool designed to enhance team collaboration and boost productivity.',
+//     userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
+//   },
+// ]
 
-export const seedFn = (prisma: ExtendedPrismaClient) =>
-  posts.map(async (data: Prisma.PostCreateArgs['data']) => {
-    const record = await prisma.post.upsert({
-      where: { id: data.id },
-      update: data,
-      create: data,
-    })
+// export const seedFn = (prisma: ExtendedPrismaClient) =>
+//   posts.map(async (data: Prisma.PostCreateArgs['data']) => {
+//     const record = await prisma.post.upsert({
+//       where: { id: data.id },
+//       update: data,
+//       create: data,
+//     })
 
-    return record
-  })
+//     return record
+//   })
 
-export const deleteFn = (prisma: ExtendedPrismaClient) =>
-  posts.map(async (data: Prisma.PostCreateArgs['data']) => {
-    await prisma.post.delete({
-      where: { id: data.id },
-    })
-  })
+// export const deleteFn = (prisma: ExtendedPrismaClient) =>
+//   posts.map(async (data: Prisma.PostCreateArgs['data']) => {
+//     await prisma.post.delete({
+//       where: { id: data.id },
+//     })
+//   })
