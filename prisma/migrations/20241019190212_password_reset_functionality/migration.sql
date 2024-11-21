d522776db67156f4ef76d5a7e6fceb3317725924
Victoria Kastanenka <vkastanenka@gmail.com>
2024-11-21 14:07:48 -0500

    set up package.json

--- prisma/migrations/20241019190212_password_reset_functionality/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "password_reset_token" TEXT,
-ADD COLUMN     "password_reset_token_expires" TIMESTAMP(3),
-ADD COLUMN     "password_update_at" TIMESTAMP(3);
--- prisma/migrations/20241104212909_changed_github_repo_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `github_repositories` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "github_repositories",
-ADD COLUMN     "github_repos" TEXT[];
