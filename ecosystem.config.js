cfba0228ec24592471cfc274f46ec86b32279eff
Victoria Kastanenka <vkastanenka@gmail.com>
2024-11-21 23:20:52 -0500

    added ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+modules.exports = {
+  apps: [
+    {
+      name: 'devbook',
+      script: 'npm',
+      args: 'run dev',
+      env: {
+        NODE_ENV: 'development',
+        ENV_VAR1: 'environment-variable',
+      },
+    },
+  ],
+}
--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,8 @@ process.on('uncaughtException', (err: Error) => {
   process.exit(1)
 })
 
-const port = process.env.PORT || 5000
-const server = app.listen(port, () => {
+const port = Number(process.env.PORT) || 5000
+const server = app.listen(port, '0.0.0.0', () => {
   console.log(`App running on port ${port}!`)
 })
 
