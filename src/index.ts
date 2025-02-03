9d8be48e39009068ffd3de8f409947c4a2376314
Victoria Kastanenka <vkastanenka@gmail.com>
2025-02-03 15:40:13 -0500

    setup typescript and build

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res, next) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ status: "success!" });
 });
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
